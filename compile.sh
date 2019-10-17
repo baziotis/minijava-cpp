201f57d71a7e205dfd704c25fe880794ac20faa0
Stefanos Baziotis <sdi1600105@di.uoa.gr>
2019-10-17 18:49:10 +0300

    Test .java files

--- compile.sh
+++ compile.sh
@@ -1,3 +1,2 @@
 # Go make a coffee, we're about to compile C++.
-g++ main.cpp lex.cpp tokens.cpp str_intern.cpp error.cpp ast.cpp parse.cpp debug_print.cpp typecheck.cpp alloc.cpp
-
+g++ main.cpp lex.cpp tokens.cpp str_intern.cpp error.cpp ast.cpp parse.cpp debug_print.cpp typecheck.cpp alloc.cpp -o main
---
+++ test.java
@@ -0,0 +1,38 @@
+class stefanos {
+    public static void main(String[] a) {
+        int a;
+        if (a) {
+        } else { }
+        System.out.println(a);
+    }
+}
+
+class A extends B {
+    boolean a;
+    int b;
+
+    public int test(int c, int[] d, boolean) {
+        int c;
+        a = 3;
+        return 1;
+    }
+}
+
+class C {
+    boolean b;
+    D d2;
+    
+    public int test2(D d, int e) {
+        int c;
+        return 2;
+    }
+}
+
+class D {
+    boolean;
+    
+    public int test2(int, int d) {
+        int c;
+        return 2;
+    }
+}
