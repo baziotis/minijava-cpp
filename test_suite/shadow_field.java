7b4c8ac81a6cdf93e7532b857bc15301673ada6e
Stefanos Baziotis <sdi1600105@di.uoa.gr>
2019-10-19 23:08:40 +0300

    Test suite: Update shadown_field.java with shadowing of field inside method

--- test_suite/shadow_field.java
+++ test_suite/shadow_field.java
@@ -14,3 +14,22 @@ class A {
 class B {
     int a;
 }
+
+// Shadow field in method
+
+class C extends D {
+    int[] a;
+
+    public int[] test() {
+        return a;
+    }
+
+    public C test2() {
+        C a;
+        return a;
+    }
+}
+
+class D {
+    int a;
+}
