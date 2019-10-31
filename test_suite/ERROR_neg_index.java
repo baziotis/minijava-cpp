8adf3089bbfe5a2ed82e62156c3e7df55500ccbf
Stefanos Baziotis <sdi1600105@di.uoa.gr>
2019-10-31 16:59:47 +0200

    Constant negative index check

---
+++ test_suite/ERROR_neg_index.java
@@ -0,0 +1,13 @@
+// -unused -no-style
+class Main {	
+    public static void main(String[] a) { }	
+}	
+
+class A {	
+    public int test(int[] a) {	
+        // There's nothing like -1 (i.e. there's no unary `-`)	
+        // in MiniJava and so we have to construct -1 as a binary	
+        // expression.	
+        return a[(0 - 1)];	
+    }	
+}
